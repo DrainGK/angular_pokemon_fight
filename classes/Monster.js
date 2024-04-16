c537a1ac66e763955d473ce1da068a2c50e2a015
ロペズ ギヨム <guillaumelopezgk@hotmail.com>
2024-04-16 16:09:24 +0900

    setup

---
+++ classes/Monster.js
@@ -0,0 +1,10 @@
+class Monster{
+    constructor(){
+        this.level = 1;
+        this.point = 10;
+        this.life = 0;
+        this.attack = 0;
+        this.defense = 0;
+        this.luck = 0;
+    }
+}
